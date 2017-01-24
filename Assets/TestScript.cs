a0da71601a88be35e5cae140d28d3961109a324b
Jayden Kroes <xtra1nfinight@gmail.com>
2017-01-24 22:12:12 +1100

    Script change

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	//Test change
 }
